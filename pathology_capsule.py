5eb4a2d2725c61e55573b3190ecf7aeb35c44f19
root <tom.l.athey@gmail.com>
2018-08-22 02:34:41 +0000

    memory erro

--- pathology_capsule.py
+++ pathology_capsule.py
@@ -33,21 +33,21 @@ def CapsNet(input_shape, n_class, routings):
     """
     x = layers.Input(shape=input_shape)
     x_resize = layers.Lambda(lambda image: ktf.image.resize_bilinear(image,
-                                                         (512,512)))(x) 
+                                                         (256,256)))(x) 
     # Conv Layers
     conv1 = layers.Conv2D(filters=64, kernel_size=4, strides=2, padding='valid',
                           activation='relu', name='conv1')(x_resize)
     conv2 = layers.Conv2D(filters=128, kernel_size=4, strides=2, padding='valid',
                           activation='relu', name='conv2')(conv1)
     conv3 = layers.Conv2D(filters=256, kernel_size=6, strides=2, padding='valid',
-                          activation='relu', name='conv2')(conv2)
+                          activation='relu', name='conv3')(conv2)
     conv4 = layers.Conv2D(filters=256, kernel_size=6, strides=2, padding='valid',
-                          activation='relu', name='conv2')(conv3)
+                          activation='relu', name='conv4')(conv3)
     conv5 = layers.Conv2D(filters=256, kernel_size=8, strides=2, padding='valid',
-                          activation='relu', name='conv2')(conv4)
+                          activation='relu', name='conv5')(conv4)
 
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=3872, kernel_size=9, strides=2, padding='valid')
+    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=512, kernel_size=9, strides=2, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
     digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16,
@@ -234,7 +234,7 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # load data
-    (x_train, y_train), (x_test, y_test), label_to_class = pathology.load_data()
+    (x_train, y_train_col), (x_test, y_test_col), label_to_class = pathology.load_data()
 
     print('Reformatting data...')
     if K.image_data_format() == 'channels_first':
@@ -246,11 +246,17 @@ if __name__ == "__main__":
         x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
         input_shape = (img_rows, img_cols, 3)
 
+    y_train = np.zeros((y_train_col.shape[0],np.amax(y_train_col)+1))
+    y_train[np.arange(y_train.shape[0]),y_train_col] = 1
+    y_test = np.zeros((y_test_col.shape[0],np.amax(y_test_col)+1))
+    y_test[np.arange(y_test.shape[0]),y_test_col] = 1
+
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
     x_train /= 255
     x_test /= 255
     print('x_train shape:', x_train.shape)
+    print('y_train shape:', y_train.shape)
     print(x_train.shape[0], 'train samples')
     print(x_test.shape[0], 'test samples')
 
