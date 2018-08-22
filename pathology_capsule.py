wrong dims for fit

pathology_capsule.py
@@ -50,7 +50,7 @@ def CapsNet(input_shape, n_class, routings):
     primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=512, kernel_size=9, strides=2, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
-    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16,
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=8,
                              routings=routings,
                              name='digitcaps')(primarycaps)
 
@@ -58,30 +58,33 @@ def CapsNet(input_shape, n_class, routings):
     # If using tensorflow, this will not be necessary. :)
     out_caps = Length(name='capsnet')(digitcaps)
 
+    
     # Decoder network.
     y = layers.Input(shape=(n_class,))
+    '''
     masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
     masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
-
+    
     # Shared Decoder model in training and prediction
     decoder = models.Sequential(name='decoder')
     decoder.add(layers.Dense(512, activation='relu', input_dim=16*n_class))
     decoder.add(layers.Dense(1024, activation='relu'))
     decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
     decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
-
+    '''
     # Models for training and evaluation (prediction)
-    train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
-    eval_model = models.Model(x, [out_caps, decoder(masked)])
+    train_model = models.Model([x, y], [out_caps])
+    eval_model = models.Model(x, [out_caps])
 
 
     # manipulate model
+    '''
     noise = layers.Input(shape=(n_class, 16))
     noised_digitcaps = layers.Add()([digitcaps, noise])
     masked_noised_y = Mask()([noised_digitcaps, y])
-    manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
-  
-    return train_model, eval_model, manipulate_model
+    manipulate_model = models.Model([x, y, noise])
+    '''
+    return train_model, eval_model
 
 
 def margin_loss(y_true, y_pred):
@@ -118,8 +121,8 @@ def train(model, data, args):
 
     # compile the model
     model.compile(optimizer=optimizers.Adam(lr=args.lr),
-                  loss=[margin_loss, 'mse'],
-                  loss_weights=[1., args.lam_recon],
+                  loss=[margin_loss],
+                  loss_weights=[1.],
                   metrics={'capsnet': 'accuracy'})
 
     """
@@ -134,14 +137,14 @@ def train(model, data, args):
                                            height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
         generator = train_datagen.flow(x, y, batch_size=batch_size)
         while 1:
-            x_batch, y_batch = generator.next()
-            yield ([x_batch, y_batch], [y_batch, x_batch])
+            yield generator.next()
 
     # Training with data augmentation. If shift_fraction=0., also no augmentation.
+    
     model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
                         steps_per_epoch=int(y_train.shape[0] / args.batch_size),
                         epochs=args.epochs,
-                        validation_data=[[x_test, y_test], [y_test, x_test]],
+                        validation_data=(x_test, y_test),
                         callbacks=[log, tb, checkpoint, lr_decay])
     # End: Training with data augmentation -----------------------------------------------------------------------#
 
@@ -156,18 +159,10 @@ def train(model, data, args):
 
 def test(model, data, args):
     x_test, y_test = data
-    y_pred, x_recon = model.predict(x_test, batch_size=100)
+    y_pred = model.predict(x_test, batch_size=100)
     print('-'*30 + 'Begin: test' + '-'*30)
     print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1))/y_test.shape[0])
 
-    img = combine_images(np.concatenate([x_test[:50],x_recon[:50]]))
-    image = img * 255
-    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
-    print()
-    print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
-    print('-' * 30 + 'End: test' + '-' * 30)
-    plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
-    plt.show()
 
 
 def manipulate_latent(model, data, args):
@@ -212,7 +207,7 @@ if __name__ == "__main__":
                         help="Initial learning rate")
     parser.add_argument('--lr_decay', default=0.9, type=float,
                         help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
-    parser.add_argument('--lam_recon', default=0.392, type=float,
+    parser.add_argument('--lam_recon', default=0, type=float,
                         help="The coefficient for the loss of decoder")
     parser.add_argument('-r', '--routings', default=3, type=int,
                         help="Number of iterations used in routing algorithm. should > 0")
@@ -245,11 +240,14 @@ if __name__ == "__main__":
         x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
         x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
         input_shape = (img_rows, img_cols, 3)
-
+    
+    #onehot
+    
     y_train = np.zeros((y_train_col.shape[0],np.amax(y_train_col)+1))
     y_train[np.arange(y_train.shape[0]),y_train_col] = 1
     y_test = np.zeros((y_test_col.shape[0],np.amax(y_test_col)+1))
     y_test[np.arange(y_test.shape[0]),y_test_col] = 1
+    
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
@@ -262,8 +260,8 @@ if __name__ == "__main__":
 
 
     # define model
-    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
-                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
+    model, eval_model = CapsNet(input_shape=x_train.shape[1:],
+                                                  n_class=len(np.unique(np.argmax(y_train,1))),
                                                   routings=args.routings)
     model.summary()
 
@@ -275,5 +273,5 @@ if __name__ == "__main__":
     else:  # as long as weights are given, will run testing
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
-        manipulate_latent(manipulate_model, (x_test, y_test), args)
+        #manipulate_latent(manipulate_model, (x_test, y_test), args)
         test(model=eval_model, data=(x_test, y_test), args=args)